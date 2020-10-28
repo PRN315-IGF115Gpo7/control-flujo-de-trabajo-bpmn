b870dd0a30412ca801e0d07fa454f05eec05e66e
Pablo Diaz <diazcolato1997@gmail.com>
2020-10-27 18:37:47 -0600

    Implementación de Spring Security

--- src/main/java/com/gpo7/proceso/controller/BaseController.java
+++ src/main/java/com/gpo7/proceso/controller/BaseController.java
@@ -1,11 +1,13 @@
 package com.gpo7.proceso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/flujo-de-trabajo")
+@RequestMapping("/")
 public class BaseController {
 	
 	@GetMapping("/index")
@@ -16,6 +18,23 @@ public class BaseController {
 	@GetMapping("/base")
 	public String base() {
 		return "base";
+	}
+	
+	@GetMapping("/login")
+	public String showLoginForm(Model model,
+			@RequestParam(name="error", required = false) String error,
+			@RequestParam(name = "logout", required = false) String logout) {
+		
+		model.addAttribute("error", error);
+		model.addAttribute("logout", logout);
+
+		return "login";
+	}
+	
+	@GetMapping({"/loginsuccess","/"})
+	public String loginCheck() {
+		
+		return "redirect:/index";
 	}
 
 }
